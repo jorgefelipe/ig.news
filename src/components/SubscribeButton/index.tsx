2d19ea3474cb7475d2ae782d7b208bcc4f496225
jorgefelipe <jorgefelipecunha@gmail.com>
2021-05-05 23:04:08 -0300

    Adiciona pagina de curso

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,46 @@
+import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+import styles from './styles.module.scss';
+
+interface SubscribeButtonProps{
+    priceId: string;
+}
+
+export function SubscribeButton({priceId}: SubscribeButtonProps){
+    const [session] = useSession();
+
+    async function handleSubscribe(){
+        console.log("passou-1");
+        if(!session){
+            console.log("passou0")
+            signIn('github')  
+            return;
+        }
+        
+        try{
+            console.log("passou1");
+            const response = await api.post('/subscribe');
+            console.log("passou2");
+            const {sessionId} = response.data;
+
+            const stripe = await getStripeJs();
+            console.log("passou3");
+            await stripe.redirectToCheckout({sessionId});
+            
+        } catch(err){
+            alert(err.message);
+        }
+
+    }
+
+    return(
+        <button 
+            type="button" 
+            className={styles.subscribeButton}
+            onClick={() =>handleSubscribe()}
+        >
+            Subscribe now
+        </button>
+    );
+}
